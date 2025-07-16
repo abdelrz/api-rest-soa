0bfd4ad7fb513b8510a9f05d048fd247fdebccf8
abdelrz <102103267+abdelrz@users.noreply.github.com>
2025-07-16 15:56:28 -0500

    FEAT: Se añadieron algunos métodos en la implementación de servicio adicional y ticket soporte

--- src/main/java/com/proyecto/soa/servicio/impl/ServicioAdicionalServicioImpl.java
+++ src/main/java/com/proyecto/soa/servicio/impl/ServicioAdicionalServicioImpl.java
@@ -1,7 +1,42 @@
 package com.proyecto.soa.servicio.impl;
 
+import com.proyecto.soa.controlador.dto.ServicioAdicionalDTO;
+import com.proyecto.soa.entidad.ServicioAdicional;
+import com.proyecto.soa.persistencia.IServicioAdicionalDAO;
+import com.proyecto.soa.servicio.IServicioAdicionalServicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class ServicioAdicionalServicioImpl implements IServicioAdicionalServicio {
+    @Autowired
+    private IServicioAdicionalDAO servicioAdicionalDAO;
 
+    @Override
+    public List<ServicioAdicional> encontrarTodos() {
+        return servicioAdicionalDAO.encontrarTodos();
+    }
+
+    @Override
+    public Optional<ServicioAdicional> encontrarPorId(Long id) {
+        return servicioAdicionalDAO.encontrarPorId(id);
+    }
+
+    @Override
+    public void guardar(ServicioAdicional servicioAdicional) {
+        servicioAdicionalDAO.guardar(servicioAdicional);
+    }
+
+    @Override
+    public void eliminarPorId(Long id) {
+        servicioAdicionalDAO.eliminarPorId(id);
+    }
 }
--- src/main/java/com/proyecto/soa/servicio/impl/TicketSoporteServicioImpl.java
+++ src/main/java/com/proyecto/soa/servicio/impl/TicketSoporteServicioImpl.java
@@ -1,7 +1,42 @@
 package com.proyecto.soa.servicio.impl;
 
+import com.proyecto.soa.controlador.dto.TicketSoporteDTO;
+import com.proyecto.soa.entidad.TicketSoporte;
+import com.proyecto.soa.persistencia.ITicketSoporteDAO;
+import com.proyecto.soa.servicio.ITicketSoporteServicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class TicketSoporteServicioImpl implements ITicketSoporteServicio {
+    @Autowired
+    private ITicketSoporteDAO ticketSoporteDAO;
 
+    @Override
+    public List<TicketSoporte> encontrarTodos() {
+        return ticketSoporteDAO.encontrarTodos();
+    }
+
+    @Override
+    public Optional<TicketSoporte> encontrarPorId(Long id) {
+        return ticketSoporteDAO.encontrarPorId(id);
+    }
+
+    @Override
+    public void guardar(TicketSoporte ticketSoporte) {
+        ticketSoporteDAO.guardar(ticketSoporte);
+    }
+
+    @Override
+    public void eliminarPorId(Long id) {
+        ticketSoporteDAO.eliminarPorId(id);
+    }
 }
